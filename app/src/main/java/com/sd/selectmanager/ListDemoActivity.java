07d69906e36eb2d74c349123dce0df6608d29653
zjun <565061763@qq.com>
2020-09-30 16:21:39 +0800

    androidx

--- app/src/main/java/com/sd/selectmanager/ListDemoActivity.java
+++ app/src/main/java/com/sd/selectmanager/ListDemoActivity.java
@@ -1,7 +1,7 @@
 package com.sd.selectmanager;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.widget.ListView;
 import android.widget.TextView;
